9d31eb7676a890e07814aa06b95b55fee28a8b54
albertoasensiom <albertoasensiom@gmail.com>
2024-07-08 19:29:40 +0100

    Update Main.java

--- desktop/src/com/mygdx/mygame/Main.java
+++ desktop/src/com/mygdx/mygame/Main.java
@@ -5,11 +5,11 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class Main {
     public static void main(String[] arg) {
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setForegroundFPS(60);
-        config.setTitle("Cosmo Music");
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration(); // Create LWJGL3 application configuration
+        config.setForegroundFPS(60); // Set the frame rate to 60 FPS
+        config.setTitle("Cosmo Music"); // Set the window title
         config.setWindowedMode(800, 600); // Initial window size
-        config.setResizable(false);
-        new Lwjgl3Application(new MyGame(), config);
+        config.setResizable(false); // Make the window non-resizable
+        new Lwjgl3Application(new MyGame(), config); // Create and start a new application with the given configuration
     }
 }
